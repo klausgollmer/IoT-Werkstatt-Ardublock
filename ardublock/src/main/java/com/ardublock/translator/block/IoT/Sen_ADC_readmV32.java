b0e4ad001c6247dc569cad5474280ef6ebd6de25
Klaus <k.gollmer@umwelt-campus.de>
2025-07-27 12:36:07 +0200

    analog read in mV

---
+++ ardublock/src/main/java/com/ardublock/translator/block/IoT/Sen_ADC_readmV32.java
@@ -0,0 +1,23 @@
+package com.ardublock.translator.block.IoT;
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Sen_ADC_readmV32 extends TranslatorBlock
+{
+
+  public Sen_ADC_readmV32 (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+  {
+    super(blockId, translator, codePrefix, codeSuffix, label);
+  }
+
+  public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+  {
+	    String ret = "analogReadMilliVolts(A";
+		TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		ret = ret + translatorBlock.toCode();
+		ret = ret + ")";
+		return codePrefix + ret + codeSuffix;
+  }
+}
