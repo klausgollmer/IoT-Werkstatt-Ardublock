4d3ba34863a0238527e14522a8cb5918553ede8f
Klaus <k.gollmer@umwelt-campus.de>
2025-09-02 20:47:40 +0200

    Metronom

---
+++ ardublock/src/main/java/com/ardublock/translator/block/IoT/Sen_ButtonRotMinMax2.java
@@ -0,0 +1,51 @@
+package com.ardublock.translator.block.IoT;
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class Sen_ButtonRotMinMax2 extends TranslatorBlock
+{
+
+  public Sen_ButtonRotMinMax2 (Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+  {
+    super(blockId, translator, codePrefix, codeSuffix, label);
+  }
+
+	
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{
+		 String ret;
+		 TranslatorBlock translatorBlock = this.getRequiredTranslatorBlockAtSocket(1);
+		 String min = translatorBlock.toCode();
+		 translatorBlock = this.getRequiredTranslatorBlockAtSocket(2);
+		 String max = translatorBlock.toCode();
+		 translatorBlock = this.getRequiredTranslatorBlockAtSocket(0);
+		 String start = translatorBlock.toCode();
+ 
+		  String EncDef =   "#if defined(ESP8266) \n"
+			      + "// Autor Paul Stoffregen, http://www.pjrc.com/teensy/td_libs_Encoder.html\n"
+			      + " #include <Encoder.h> \n"
+			      + "#elif defined(ESP32) \n"
+			      + "// Autor: Kevin Harrington, https://www.arduino.cc/reference/en/libraries/esp32encoder\n"
+			      + " #include <ESP32Encoder.h>;\n"
+			      + "#endif\n"
+			      + "// encoder max range\n"
+			      + "int encoder_counter_rot_min = -100;\n"
+			      + "int encoder_counter_rot_max = 100;\n"
+			      + "";
+	     translator.addDefinitionCommand(EncDef);
+   	     translator.addDefinitionCommand(EncDef);
+		  
+		 ret = "// Configure upper/lower bounds encoder\r\n" +
+		    		    "encoder_counter_rot_min="+min+";\n"+
+		    		    "encoder_counter_rot_max="+max+";\n" +
+		    		    "#if defined(ESP32)\n"+
+		    		    "  button_encoder.setCount("+start+"*2);\n"+
+		    		    "#else\n"+
+		    		    "  button_encoder.write("+start+");"+
+		    		    "#endif\n";
+		 
+		 return codePrefix + ret + codeSuffix;
+	}
+}
